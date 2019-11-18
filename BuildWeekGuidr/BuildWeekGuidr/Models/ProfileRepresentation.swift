0e04159519e217fbc885122cfc78afcbff81a341
Dan M <djmthedev@gmail.com>
2019-11-18 15:07:21 -0800

    added ProfileRepresentation

---
+++ BuildWeekGuidr/BuildWeekGuidr/Models/ProfileRepresentation.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileRepresentation.swift
+//  BuildWeekGuidr
+//
+//  Created by morse on 11/18/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct ProfileRepresentation: Codable {
+    let age: Int16
+    let guideSpecialty: String
+    let password: String
+    let tagline: String
+    let title: String
+    let username: String
+    let yearsExperience: Int16
+}
