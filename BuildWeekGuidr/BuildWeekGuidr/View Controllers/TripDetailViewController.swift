4ad91b01ca32c4cd9fe42d000711f0febd7aafc9
Dan M <djmthedev@gmail.com>
2019-11-18 12:05:37 -0800

    connects outlets for TripDetailViewController

--- BuildWeekGuidr/BuildWeekGuidr/View Controllers/TripDetailViewController.swift
+++ BuildWeekGuidr/BuildWeekGuidr/View Controllers/TripDetailViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class TripDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var privateLabel: UILabel!
+    @IBOutlet weak var professionalLabel: UILabel!
+    
+    // MARK: - Properties
+    var trip: Trip?
+    
+    // MARK: - Lifecycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
