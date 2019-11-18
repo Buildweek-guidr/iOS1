7134f3e97295f9d421041ca6350391346300f7b1
Dan M <djmthedev@gmail.com>
2019-11-18 15:55:55 -0800

    added TripRepresentation

---
+++ BuildWeekGuidr/BuildWeekGuidr/Models/TripRepresentation.swift
@@ -0,0 +1,28 @@
+//
+//  TripRepresentation.swift
+//  BuildWeekGuidr
+//
+//  Created by morse on 11/18/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct TripRepresentation: Codable {
+    let title: String
+    let tripDescription: String
+    let isPrivate: Int
+    let isProfessional: Int
+    let image: String
+    let duration: Int // In days
+    let distance: Int // In miles
+    let date: String // YYYY-MM-DD
+    let tripType: String
+    let userId: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case title, isPrivate, isProfessional, image, duration, distance, date, tripType
+        case userId = "user_id"
+        case tripDescription = "description"
+    }
+}
